74f01b66e796486274028054ef7437127f11a9b9
Ravinder Saluja <ravz.saluja@yahoo.com>
2020-04-08 23:00:23 +0530

    Add file with KFCV

---
+++ ann with kfcv.py
@@ -0,0 +1,53 @@
+# Importing the libraries
+import numpy as np
+import pandas as pd
+
+# Importing the dataset
+df = pd.read_csv('Churn_Modelling.csv')
+X = df.iloc[:, 3:13]
+y = df.iloc[:, 13]
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Dummy Variable Generation
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+column_trans = make_column_transformer((OneHotEncoder(drop='first'), ['Geography', 'Gender'])
+                                       , remainder='passthrough')
+X_train = column_trans.fit_transform(X_train)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+
+# Transforming the test data as well
+X_test = column_trans.transform(X_test)
+X_test = sc.transform(X_test)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1)
+mean = accuracies.mean()
+variance = accuracies.std()
+print("Mean of accuracies:", mean)
+print("Variance:", variance)
+
+
+
